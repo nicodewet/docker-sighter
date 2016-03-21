faba377571de6810f8f0d06c96606b6b7cbdfa77
Nico de Wet <nico@nicodewet.com>
2016-03-22 01:15:38 +1300

    docker engine install in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,3 +18,16 @@ export PATH=$PATH:/usr/local/go/bin
 export GOPATH=/vagrant/
 go install hello
 /vagrant/bin/hello
+
+sudo dnf -y update
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/fedora/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+sudo dnf -y install docker-engine
+sudo systemctl start docker
+
